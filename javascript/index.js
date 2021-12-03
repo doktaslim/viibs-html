f28393499815d9ab15a919d4a9955809f7855c24
KellsCodes <nworjiifeanyi@gmail.com>
2021-12-03 08:08:05 +0100

    started work on the sign up page

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,9 @@
 console.log('Hello World');
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+  <p>And all who inhabit it</p>
+`;
+
+document.body.appendChild(template.content);
