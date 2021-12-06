c73b6eb43813fc0be5ad5b0d11f8cbe90e27d02e
KellsCodes <nworjiifeanyi@gmail.com>
2021-12-06 19:46:30 +0100

    update

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ import users from "../jsondata/cardDetails.js"
 
 users.map(user => {
     return document.querySelector('#cards').innerHTML += `
-        <div id='card' class="w-full card h-96 max-h-96 rounded">
+        <div id='card' class="w-full card controller1list h-96 max-h-96 rounded">
             <div class="flex flex-col">
                 <img src=${user.img} alt="profile" id='userImg' class="rounded w-full h-60 max-h-60 object-cover">
                 <div class="space-y-1 card-details">
@@ -22,7 +22,7 @@ users.map(user => {
 
 users.map(user => {
     return document.querySelector('#actors').innerHTML += `
-        <div id='card' class="w-full card h-96 max-h-96 rounded">
+        <div id='card' class="w-full card controller2list h-96 max-h-96 rounded">
             <div class="flex flex-col">
                 <img src=${user.img} alt="profile" id='userImg' class="rounded w-full h-60 max-h-60 object-cover">
                 <div class="space-y-1 card-details">
@@ -42,7 +42,7 @@ users.map(user => {
 
 users.map(user => {
     return document.querySelector('#creators').innerHTML += `
-        <div id='card' class="w-full card h-96 max-h-96 rounded">
+        <div id='card' class="w-full card controller3list h-96 max-h-96 rounded">
             <div class="flex flex-col">
                 <img src=${user.img} alt="profile" id='userImg' class="rounded w-full h-60 max-h-60 object-cover">
                 <div class="space-y-1 card-details">
@@ -61,7 +61,7 @@ users.map(user => {
 
 users.map(user => {
     return document.querySelector('#musicians').innerHTML += `
-        <div id='card' class="w-full card h-96 max-h-96 rounded">
+        <div id='card' class="w-full card controller4list h-96 max-h-96 rounded">
             <div class="flex flex-col">
                 <img src=${user.img} alt="profile" id='userImg' class="rounded w-full h-60 max-h-60 object-cover">
                 <div class="space-y-1 card-details">
@@ -79,42 +79,42 @@ users.map(user => {
 });
 
 let span = document.querySelectorAll('#span');
-let card = document.querySelectorAll('#card');
-let card_page = Math.ceil(card.length/4);
-let l = 0;
-let movePer = 25.34;
-let maxMove = 203;
-// mobile_view	
-let mob_view = window.matchMedia("(max-width: 768px)");
-if (mob_view.matches){
-    movePer = 50.36;
-    maxMove = 504;
-}
+// let card = document.querySelectorAll('#card');
+// let card_page = Math.ceil(card.length/4);
+// let l = 0;
+// let movePer = 25.34;
+// let maxMove = 203;
+// // mobile_view	
+// let mob_view = window.matchMedia("(max-width: 768px)");
+// if (mob_view.matches){
+//     movePer = 50.36;
+//     maxMove = 504;
+// }
 
-let mob_view1 = window.matchMedia("(max-width: 500px)");
-if (mob_view1.matches){
-    movePer = 75.38;
-    maxMove = 1005;
-}
+// let mob_view1 = window.matchMedia("(max-width: 500px)");
+// if (mob_view1.matches){
+//     movePer = 75.38;
+//     maxMove = 1005;
+// }
 
-let right_mover = ()=>{
-    l = l + movePer;
-    if (card == 1){l = 0; }
-    for(const i of card)
-    {
-        if (l > maxMove){l = l - movePer;}
-        i.style.left = '-' + l + '%';
-    }
+// let right_mover = ()=>{
+//     l = l + movePer;
+//     if (card == 1){l = 0; }
+//     for(const i of card)
+//     {
+//         if (l > maxMove){l = l - movePer;}
+//         i.style.left = '-' + l + '%';
+//     }
 
-}
-let left_mover = ()=>{
-    l = l - movePer;
-    if (l<=0){l = 0;}
-    for(const i of card){
-        if (card_page>1){
-            i.style.left = '-' + l + '%';
-        }
-    }
-}
-span[1].onclick = ()=>{right_mover();}
-span[0].onclick = ()=>{left_mover();}
+// }
+// let left_mover = ()=>{
+//     l = l - movePer;
+//     if (l<=0){l = 0;}
+//     for(const i of card){
+//         if (card_page>1){
+//             i.style.left = '-' + l + '%';
+//         }
+//     }
+// }
+// span[1].onclick = ()=>{right_mover();}
+// span[0].onclick = ()=>{left_mover();}
