2c9c2879e8e79886c9670163f83216c63ee0045e
doktaslim <gozzypat@gmail.com>
2021-12-07 07:03:20 +0100

    update

--- javascript/index.js
+++ javascript/index.js
@@ -78,43 +78,43 @@ users.map(user => {
     `
 });
 
-let span = document.querySelectorAll('#span');
-let card = document.querySelectorAll('#card');
-let card_page = Math.ceil(card.length/4);
-let l = 0;
-let movePer = 25.34;
-let maxMove = 203;
-// mobile_view	
-let mob_view = window.matchMedia("(max-width: 768px)");
-if (mob_view.matches){
-    movePer = 50.36;
-    maxMove = 504;
-}
+// let span = document.querySelectorAll('#span');
+// let card = document.querySelectorAll('#card');
+// let card_page = Math.ceil(card.length/4);
+// let l = 0;
+// let movePer = 25.34;
+// let maxMove = 203;
+// // mobile_view	
+// let mob_view = window.matchMedia("(max-width: 768px)");
+// if (mob_view.matches){
+//     movePer = 50.36;
+//     maxMove = 504;
+// }
 
-let mob_view1 = window.matchMedia("(max-width: 500px)");
-if (mob_view1.matches){
-    movePer = 75.38;
-    maxMove = 1005;
-}
+// let mob_view1 = window.matchMedia("(max-width: 500px)");
+// if (mob_view1.matches){
+//     movePer = 75.38;
+//     maxMove = 1005;
+// }
 
-let right_mover = ()=>{
-    l = l + movePer;
-    if (card == 1){l = 0; }
-    for(const i of card)
-    {
-        if (l > maxMove){l = l - movePer;}
-        i.style.left = '-' + l + '%';
-    }
+// let right_mover = ()=>{
+//     l = l + movePer;
+//     if (card == 1){l = 0; }
+//     for(const i of card)
+//     {
+//         if (l > maxMove){l = l - movePer;}
+//         i.style.left = '-' + l + '%';
+//     }
 
-}
-let left_mover = ()=>{
-    l = l - movePer;
-    if (l<=0){l = 0;}
-    for(const i of card){
-        if (card_page>1){
-            i.style.left = '-' + l + '%';
-        }
-    }
-}
-span[1].onclick = ()=>{right_mover();}
-span[0].onclick = ()=>{left_mover();}
+// }
+// let left_mover = ()=>{
+//     l = l - movePer;
+//     if (l<=0){l = 0;}
+//     for(const i of card){
+//         if (card_page>1){
+//             i.style.left = '-' + l + '%';
+//         }
+//     }
+// }
+// span[1].onclick = ()=>{right_mover();}
+// span[0].onclick = ()=>{left_mover();}
